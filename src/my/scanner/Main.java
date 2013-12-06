e29a70a7d0630279fdfe046053711c05ab97528a
syamka <syamka@bk.ru>
2013-12-06 20:04:26 +0400

    Первый коммит

---
+++ src/my/scanner/Main.java
@@ -0,0 +1,24 @@
+/*
+* (C) Copyright 1997 i-Teco, CJSK. All Rights reserved.
+* i-Teco PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+*
+* Эксклюзивные права 1997 i-Teco, ЗАО.
+* Данные исходные коды не могут использоваться и быть изменены
+* без официального разрешения компании i-Teco.          
+*/
+package my.scanner;
+
+import java.io.IOException;
+
+/**
+ * <h3></h3>
+ * <p></p>
+ * <p>Author: predtechenskaya (predtechenskaya@i-teco.ru)</p>
+ * <p>Date: 03.12.13</p>
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new Processor(new Configuration(".", ".")).process();
+    }
+}
